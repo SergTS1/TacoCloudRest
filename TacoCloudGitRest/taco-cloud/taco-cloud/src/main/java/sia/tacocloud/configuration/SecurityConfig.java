46cd24985c2e6bfe9983122236617b5e1fed65b0
sergey <zak_on@list.ru>
2025-03-23 23:18:50 +0300

    Security 3

--- TacoCloudGitRest/taco-cloud/taco-cloud/src/main/java/sia/tacocloud/configuration/SecurityConfig.java
+++ TacoCloudGitRest/taco-cloud/taco-cloud/src/main/java/sia/tacocloud/configuration/SecurityConfig.java
@@ -4,25 +4,16 @@ package sia.tacocloud.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.context.annotation.RequestScope;
 import sia.tacocloud.domain.entyty.User;
 import sia.tacocloud.domain.reposytory.UserRepository;
 import sia.tacocloud.security.UserDetailsService;
-import sia.tacocloud.service.IngredientService;
-import sia.tacocloud.service.RestIngredientService;
 
 @Configuration
 public class SecurityConfig {
@@ -41,59 +32,27 @@ public class SecurityConfig {
         };
     }
 
+
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .oauth2Login(oauth2 -> oauth2.loginPage("/oath2/authorization/taco-admin-client"))
-                .oauth2Client(Customizer.withDefaults());
-        return http.build();
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri("http://localhost:9000/oauth2/jwks.json").build();
     }
 
     @Bean
-    @RequestScope
-    public IngredientService ingredientService(
-            OAuth2AuthorizedClientService clientService) {
-        Authentication authentication =
-                SecurityContextHolder.getContext().getAuthentication();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/ingredients")
+                        .hasAuthority("SCOPE_writeIngredients")
+                        .requestMatchers(HttpMethod.DELETE, "/api/ingredients")
+                        .hasAuthority("SCOPE_deleteIngredients")
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder())))
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions().disable())
+                .build();
 
-        String accessToken = null;
-
-        if (authentication.getClass()
-                .isAssignableFrom(OAuth2AuthenticationToken.class)) {
-            OAuth2AuthenticationToken oauthToken =
-                    (OAuth2AuthenticationToken) authentication;
-            String clientRegistrationId =
-                    oauthToken.getAuthorizedClientRegistrationId();
-            if (clientRegistrationId.equals("taco-admin-client")) {
-                OAuth2AuthorizedClient client =
-                        clientService.loadAuthorizedClient(
-                                clientRegistrationId, oauthToken.getName());
-                accessToken = client.getAccessToken().getTokenValue();
-            }
-        }
-        return new RestIngredientService(accessToken);
     }
-
-//    @Bean
-//    public JwtDecoder jwtDecoder() {
-//        return NimbusJwtDecoder.withJwkSetUri("http://localhost:9000/oauth2/jwks.json").build();
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        return http
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/h2-console/**").permitAll()
-//                        .requestMatchers(HttpMethod.POST, "/api/ingredients")
-//                        .hasAuthority("SCOPE_writeIngredients")
-//                        .requestMatchers(HttpMethod.DELETE, "/api/ingredients")
-//                        .hasAuthority("SCOPE_deleteIngredients")
-//                        .anyRequest().authenticated()
-//                )
-//                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder())))
-//                .csrf(csrf -> csrf.disable())
-//                .headers(headers -> headers.frameOptions().disable())
-//                .build();
-//
-//    }
 }
